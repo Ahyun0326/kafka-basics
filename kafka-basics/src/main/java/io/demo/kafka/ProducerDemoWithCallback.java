ef3ac416f9e4e6d5e5efff172ec5c0ff05d59153
Yeseo Kim <chinkl850@gmail.com>
2025-05-19 21:24:04 +0900

    Feat: Java Producer 콜백 사용

---
+++ kafka-basics/src/main/java/io/demo/kafka/ProducerDemoWithCallback.java
@@ -0,0 +1,77 @@
+package io.demo.kafka;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemoWithCallback {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemoWithCallback.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("I am a Kafka Producer!");
+
+        // create Producer Properties
+        Properties properties = new Properties();
+
+        // connect to Localhost
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+
+        // set Producer Properties
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+//        properties.setProperty("batch.size", "400");
+
+        // create the Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        for (int j = 0; j < 10; j++) {
+
+            for (int i = 0; i < 30; i++) {
+
+                // create the Producer Record
+                ProducerRecord<String, String> producerRecord =
+                        new ProducerRecord<>("demo_java", "hello world " + i);
+
+                // send data
+                producer.send(producerRecord, new Callback() {
+                    @Override
+                    public void onCompletion(RecordMetadata metadata, Exception e) {
+                        // executes every time a record successfully sent or on exception is thrown
+                        // record가 성공적으로 전송되거나 예외가 발생할 때마다 해당 콜백 함수 실행
+                        if (e == null) {
+                            // the record was successfully sent
+                            log.info("Received new metadata \n" +
+                                    "Topic: " + metadata.topic() + "\n" +
+                                    "Partition: " + metadata.partition() + "\n" +
+                                    "Offset: " + metadata.offset() + "\n" +
+                                    "Timestamp: " + metadata.timestamp());
+                        } else {
+                            log.error("Error while producing", e);
+                        }
+                    }
+                });
+            }
+
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+
+        // tell the producer to send all data and block until done -- synchronous (비동기식)
+        producer.flush();
+
+        // flush and close the producer
+        producer.close();
+    }
+}
