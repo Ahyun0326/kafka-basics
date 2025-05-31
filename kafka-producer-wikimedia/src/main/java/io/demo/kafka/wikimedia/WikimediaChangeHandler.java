caaac6932e80864a578cc449faae0c1663c265b3
Yeseo Kim <chinkl850@gmail.com>
2025-05-31 20:49:41 +0900

    Feat: WikimeadiaProducer 구현

---
+++ kafka-producer-wikimedia/src/main/java/io/demo/kafka/wikimedia/WikimediaChangeHandler.java
@@ -0,0 +1,50 @@
+package io.demo.kafka.wikimedia;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.MessageEvent;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+// 스트림이 새로운 메시지를 받을 때마다 호출된다.
+public class WikimediaChangeHandler implements EventHandler {
+
+    KafkaProducer<String, String> kafkaProducer;
+    String topic;
+    private final Logger log = LoggerFactory.getLogger(WikimediaChangeHandler.class.getSimpleName());
+
+    public WikimediaChangeHandler(KafkaProducer<String, String> kafkaProducer, String topic) {
+        this.kafkaProducer = kafkaProducer;
+        this.topic = topic;
+    }
+
+    @Override
+    public void onOpen() {
+        // nothing here
+    }
+
+    @Override
+    public void onClosed() {
+        kafkaProducer.close();
+    }
+
+    @Override
+    public void onMessage(String s, MessageEvent messageEvent) {
+        log.info(messageEvent.getData());
+
+        // asynchronous
+        kafkaProducer.send(new ProducerRecord<>(topic, messageEvent.getData()));
+
+    }
+
+    @Override
+    public void onComment(String s) {
+        // nothing here
+    }
+
+    @Override
+    public void onError(Throwable t) {
+        log.error("Error in Stream Reading", t);
+    }
+}
